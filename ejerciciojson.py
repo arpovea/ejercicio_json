052fc691ea44497eaddbff1baa24d447f6ca9b22
Adri <arodriguezpovea@gmail.com>
2020-03-04 17:33:56 +0100

    Ajustes finales.

--- ejerciciojson.py
+++ ejerciciojson.py
@@ -2,6 +2,9 @@ import json
 from funciones import parche
 from funciones import campeones
 from funciones import descripciones
+from funciones import stats
+from funciones import roles
+
 with open("champion.json") as fichero:
 	doc=json.load(fichero)
 print (type(doc))
@@ -25,21 +28,17 @@ while True:
 		for elem in campeones(doc):
 			print (elem)
 	elif opcion==3:
-		campeon=str(input("Dime el nombre de un campeon:"))
+		campeon=str(input("Dime el nombre de un campeon: "))
 		print("Descripción: ",descripciones(campeon,doc))
 	elif opcion==4:
-		campeon=str(input("Dime el nombre de un campeon:"))
+		campeon=str(input("Dime el nombre de un campeon: "))
 		print("Foto campeon:","http://ddragon.leagueoflegends.com/cdn/%s/img/champion/%s.png"%(doc["version"],campeon))
-		for elem in doc["data"]:
-			if elem == campeon:
-				estadisticas=doc["data"][campeon]["stats"]
-		for clave,valor in estadisticas.items():
+		for clave,valor in stats(campeon,doc).items():
 			print(clave,"->",valor)
 	elif opcion==5:
 		rol=str(input("Dime un rol: "))
-		for elem in doc["data"].values():
-			if rol in elem["tags"]:
-				print (elem["name"],elem["title"])
+		for elem in roles(rol,doc):
+			print (elem["nombre"],elem["titulo"])
 	elif opcion==0:
 		print ("¡Hasta la proxima!")
 		break
--- funciones.py
+++ funciones.py
@@ -14,3 +14,19 @@ def descripciones(campeon,doc):
 		if elem == campeon:
 			descripcion=doc["data"][campeon]["blurb"]
 	return descripcion
+
+def stats(campeon,doc):
+	for elem in doc["data"]:
+		if elem == campeon:
+			estadisticas=doc["data"][campeon]["stats"]
+	return estadisticas
+
+def roles(rol,doc):
+	listacampeones=[]
+	for elem in doc["data"].values():
+		if rol in elem["tags"]:
+			campeon={}
+			campeon["nombre"]=elem["name"]
+			campeon["titulo"]=elem["title"]
+			listacampeones.append(campeon)
+	return listacampeones
